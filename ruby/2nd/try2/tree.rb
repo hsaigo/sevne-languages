38268b36dc9c573187204cb3cca11e3cec82bc04
hsaigo <hiroch55@gmail.com>
2013-02-22 01:33:23 +0900

    Ruby2日目tree 修正

--- ruby/2nd/try2/tree.rb
+++ ruby/2nd/try2/tree.rb
@@ -1,18 +1,36 @@
 class Tree
   attr_accessor :children, :node_name
 
-  def initialize(node_name, children = nil)
-    @node_name = node_name
+  def add_children(children)
+    if children.kind_of?(Hash)
+      children.each do |child_name, grandchildren| 
+        @children << Tree.new({child_name => grandchildren})
+      end
+    elsif children.kind_of?(Array)
+      children.each do |child|
+        @children << Tree.new(child)
+      end
+    else
+      @children << Tree.new(children)
+    end
+  end
+
+  def initialize(config)
     @children = []
 
-    children.each do |child_name, grandchildren|
-      @children << Tree.new(child_name, grandchildren)
-    end if children.kind_of?(Hash)
+    unless config.kind_of?(Hash) || config.kind_of?(Array)
+      @node_name = config.to_s
+      return
+    end
 
-    children.each do |child_name|
-      @children << Tree.new(child_name)
-    end if children.kind_of?(Array)
+    if config.kind_of?(Hash) && config.count == 1
+      config.each do |name, children|
+        @node_name = name.to_s
+        config = children
+      end
+    end
 
+    add_children(config)
   end
 
   def visit_all(&block)
@@ -26,14 +44,14 @@ class Tree
 
 end
 
-ruby_tree = Tree.new("root", {
+ruby_tree = Tree.new({
   'grandpa' => { 
     'dad' => {
       'child 1' => ['child 1-1', 'child 1-2'], 
-      'child 2' => [] 
+      'child 2' => [{'child 2-1' => ['child 2-1-1', 'child 2-1-2']}, 'child 2-2'] 
     },
     'uncle' => {
-      'child 3' => [],
+      'child 3' => [['child 3-0-1', 'child 3-0-2']],
       'child 4' => ['child 4-1', 'child 4-2']
     }
   }
